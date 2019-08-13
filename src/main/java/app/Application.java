dc5dab6931420ccf3fb0e87ef0af409428c35ec7
Vladimir <v.bisenov@mail.ru>
2019-08-13 16:49:15 +0400

    randomize service-names

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -12,7 +12,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -25,29 +27,32 @@ public class Application {
     public static void main(String[] args) {
 
         int PERIOD = 1;
+        ArrayList<String> names= new ArrayList<String>();
+        names.add("First"); names.add("Second"); names.add("Third");
+        Random rnd = new Random();
 
         BufferedReader bufferedReader = getBufferedReader();
 
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.scheduleWithFixedDelay(() -> {
-            while(true){
+            int count = 0;
+            while(count < 50){
                 try {
                     String message = bufferedReader.readLine();
 
                     if (!message.equals("")) {
                         Log log = new Log();
-                        log.setDateTime(new Long(new Date().getTime()));
-                        log.setService("Temporary service");
+                        log.setDateTime(System.currentTimeMillis());
+                        log.setService(names.get(rnd.nextInt(3)));
                         log.setMessage(message);
-                        logLogger.info(message);
+                        logger.info(message);
                         LogPost.sendRequest(log);
-                        Thread.sleep(1000);
-                    } else System.out.println("Empty string");
-                } catch (IOException | InterruptedException e) {
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-        }, 0, PERIOD, TimeUnit.SECONDS);
+        }, 1, PERIOD, TimeUnit.SECONDS);
 
 
 
